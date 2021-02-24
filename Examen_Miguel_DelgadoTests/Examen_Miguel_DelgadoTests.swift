7c80462172c455f6a1e77260b49ecf8b3ce723a5
Miguel Delgado <miguel.delgado@globant.com>
2021-02-24 11:56:53 -0600

    Initial Commit

---
+++ Examen_Miguel_DelgadoTests/Examen_Miguel_DelgadoTests.swift
@@ -0,0 +1,33 @@
+//
+//  Examen_Miguel_DelgadoTests.swift
+//  Examen_Miguel_DelgadoTests
+//
+//  Created by Miguel Angel Delgado Alcantara on 24/02/21.
+//
+
+import XCTest
+@testable import Examen_Miguel_Delgado
+
+class Examen_Miguel_DelgadoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
